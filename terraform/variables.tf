c9f0aa6634efbdb1c684b8ff0bb8cfdf4670f1cc
Veljko Dencic <vdencic@vdencics-MacBook-Pro.local>
2025-04-24 00:06:41 +0200

    complete fix 2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,4 +6,27 @@ variable "ssh_public_key_path" {
   description = "Path to SSH public key"
   type        = string
 }
+# variables.tf
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
 
